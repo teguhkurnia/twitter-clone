6c4df17f6a5473887fedd87719fe7707227b7cb8
teguhkurnia <teguhkurniaixi@gmail.com>
2025-05-10 19:55:31 +0700

    fix: update PostRepositoryTrait to include create_post and find_post_replies methods

--- src/repositories/traits/post_repository.rs
+++ src/repositories/traits/post_repository.rs
@@ -1,8 +1,10 @@
 use async_trait::async_trait;
 
-use crate::models::post::Post;
+use crate::models::{dto::create_post_dto::CreatePostDto, post::Post};
 
 #[async_trait]
 pub trait PostRepositoryTrait {
     async fn find_posts_by_user_id(&self, user_id: i32) -> Result<Vec<Post>, String>;
+    async fn find_post_replies(&self, user_id: i32, post_id: i32) -> Result<Vec<Post>, String>;
+    async fn create_post(&self, user_id: i32, payload: CreatePostDto) -> Result<Post, String>;
 }
